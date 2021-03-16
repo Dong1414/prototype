56df1d1cb43913ce108e6c148f26c9f0415e977b
Dong1414 <70949409+Dong1414@users.noreply.github.com>
2021-03-16 23:01:06 +0900

    초기 셋팅

---
+++ src/pages/HomeMainPage.vue
@@ -0,0 +1,22 @@
+<template>
+ <TitleBar>홈 메인</TitleBar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: 'HomeMainPage',
+  props: {
+    globalShare: {
+      type: Object,
+      required: true
+    }
+  },
+})
+</script>
+
+<style>
+
+</style>
